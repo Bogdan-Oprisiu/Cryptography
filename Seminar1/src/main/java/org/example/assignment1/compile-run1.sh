126eabdbdb554b1c4538eb65437e1370c799fbab
Bogdan <bobo.oprisiu@gmail.com>
2025-03-16 15:35:10 +0200

    Update compile script for ex1 so that it works on linux

--- Seminar1/src/main/java/org/example/assignment1/compile-run1.sh
+++ Seminar1/src/main/java/org/example/assignment1/compile-run1.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-# Set the source file and output directory.
-SOURCE_FILE="src/main/java/org/example/Assignment1.java"
-OUT_DIR="out"
-
-# Compile the Java program.
-javac -d "$OUT_DIR" "$SOURCE_FILE"
+# Compile Utils.java and Assignment1.java into the "out" folder.
+javac -d out Assignment1.java
 if [ $? -ne 0 ]; then
     echo "Compilation failed."
     exit 1
 fi
 
-# Run the compiled program with assertions enabled (-ea).
-java -ea -cp "$OUT_DIR" org.example.Assignment1
+# Run the compiled Assignment1 program with assertions enabled (-ea).
+java -ea -cp out org.example.assignment1.Assignment1
